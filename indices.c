4b632f8c34d28b011bd8e1003731d27f1078bca4
vamcs <victoraugusto16@gmail.com>
2016-05-26 20:20:29 -0300

    Arquivo indices.c bastante modificado

--- indices.c
+++ indices.c
@@ -93,170 +93,20 @@ Link insertLink(Link list, char* course, int lineNumber){
 	return new;
 }
 
-/*FUNÇÃO AINDA INCOMPLETA.
- *Entretanto, já imprime os índices primários.
- */
-void primaryIndexes(){
-	int i = 0;		//Variável de navegação da string que contém uma linha inteira do arquivo de entrada.
-	int j = 0;		//Variável de navegação da string da chave primária.
-	int flag = 0;	//Flag indica se um espaço ' ' foi lido duas vezes seguidas.
-	int lineNumber;	//Esta variável indica o número da linha atual lida nos arquivos lista1.txt ou lista2.txt.
-	char aux;		//Variável auxiliar de leitura. Lê '\n' e garante a mudança de linha do ponteiro do arquivo.
-	char index[30];	//String que contém a CHAVE PRIMÁRIA concatenada com 30 caracteres.
-	char line[X];	//String que lê uma linha inteira do arquivo de entrada. Cada linha contem X (62) caracteres.
-	
-	/*Inicialização dos Arquivos de Entrada*/
-	FILE* input[2];
-	input[0] = fopen("lista1.txt", "r");
-	input[1] = fopen("lista2.txt", "r");
-	if(input[0] == NULL || input[0] == NULL){
-		printf("Arquivos inexistentes. Stop.\n");
-		exit(1);
-	}
-	
-	/*Inicialização dos Arquivos de Saída*/
-	FILE* output[2];
-	output[0] = fopen("indicelista1.ind", "w"); 
-	output[1] = fopen("indicelista2.ind", "w"); 
-	
-	for(lineNumber = 0;  lineNumber < Y1; lineNumber++){	//Lê as Y1 linhas do arquivo lista1.txt
-		fscanf(input[0], "%[^\n]s", line);					//Leitura da primeira linha.
-		fscanf(input[0], "%c", &aux);						//Leitura de '\n'.
-		while(j < 30 && i < X && flag < 2){					//Concatenação de 30 primeiros caracteres diferentes de ' '.
-			if(line[i] != ' '){								//Continua a concatenação enquanto j < 30, i < X e flag não indica leitura de dois espaços seguidos.
-				index[j] = line[i];							//Copia caractere da linha para a chave primária
-				i++;										//Incrementa o navegador da linha.
-				j++;										//Incrementa o navegador da chave primária.
-				flag = 0;									//Seta a flag em 0, já que não foram detectados dois ' ' seguidos.
-			}
-			else{
-				i++;										//Incrementa o navegador da linha.
-				flag++;										//Detectado 1 espaço, incrementa.
-			}
-		}
-		
-		while(j < 30){										//Caso a string da chave não tenha 30 caracteres, completa com espaços.
-			index[j] = ' ';
-			j++;
-		}
-		index[30] = '\0';									//Caractere de final da string.
-		
-		fprintf(output[0], "%s %d\n", index, lineNumber);	//Imprime: "chavePrimária NRR" no arquivo de saída.
-		i = 0;												//Reinicia variáveis de navegação e flag.
-		j = 0;
-		flag = 0;
-	}
-	
-	//O procedimento é exatamente o mesmo do arquivo de entrada 1 (lista1.txt).
-	for(lineNumber = 0; lineNumber < Y2; lineNumber++){
-		fscanf(input[1], "%[^\n]s", line);
-		fscanf(input[1], "%c", &aux);
-		while(j < 30 && i < X && flag < 2){
-			if(line[i] != ' '){
-				index[j] = line[i];
-				i++;
-				j++;
-				flag = 0;
-			}
-			else{
-				i++;
-				flag++;
-			}
-		}
-		
-		while(j < 30){
-			index[j] = ' ';
-			j++;
-		}
-		
-		index[30] = '\0';
-		
-		fprintf(output[1], "%s %d\n", index, lineNumber);
-		i = 0;
-		j = 0;
-		flag = 0;
-	}
-	
-	fclose(input[0]);
-	fclose(input[1]);
-	fclose(output[0]);
-	fclose(output[1]);
-}
-
-/*FUNÇÃO AINDA INCOMPLETA.
- *A criação da estrutura de índices secundário funciona, mas só os imprime na tela.
- *TODO: Estender a criação dos índices para o arquivo lista2.txt e imprimir nos arquivos de saída.
- *      Verificar integração com "primaryIndexes".
- */
-Link secondaryIndexes(){
-	char line[X];
-	char aux;
-	char course[2];
-	int lineNumber;
-	
-	/*Inicialização da Lista Encadeada de Cursos*/
-	Link coursesList = (Link)malloc(sizeof(*coursesList));
-	coursesList->next = NULL;
-	coursesList->first = NULL;
-	coursesList->last = NULL;
-	
-	/*Inicialização dos Arquivos de Entrada*/
-	FILE* input[2];
-	input[0] = fopen("lista1.txt", "r");
-	input[1] = fopen("lista2.txt", "r");
-	if(input[0] == NULL || input[0] == NULL){
-		printf("Arquivos inexistentes. Stop.\n");
-		exit(1);
-	}
-	
-	/*Inicialização dos Arquivos de Saída*/
-	FILE* output[2];
-	output[0] = fopen("indiceSecundariolista1.ind", "w"); 
-	output[1] = fopen("indiceSecundariolista2.ind", "w"); 
-	
-	//Leitura de todas as linhas do arquivo de entrada 1 (input[0]).
-	for(lineNumber = 0; lineNumber < Y1; lineNumber++){
-		fscanf(input[0], "%[^\n]s", line);		//Lê uma linha completa.
-		fscanf(input[0], "%c", &aux);			//Lê '\n'
-		
-		//As posições 52 e 53 contêm a string do curso.
-		course[0] = line[52];
-		course[1] = line[53];
-		course[2] = '\0';
-		
-		if(checkCourse(coursesList->next, course)){	//Verifica se o curso lido já existe na lista de cursos.
-			//Caso positivo, procura o nó que contém o curso desejado e coloca o índice primário na fila.
-			insertQueue(coursesList->next, course, lineNumber);
-		}
-		else{	//Caso o curso não exista na lista, cria-se um novo nó contendo o curso lido, incluindo o primeiro índice de ocorrência do curso no arquivo.
-			coursesList->next = insertLink(coursesList->next, course, lineNumber);
-		}
-	}
-	
-	/*Impressão da Lista de Cursos e da fila de ponteiros na tela*/
-	//Percorre toda a lista de cursos e suas respectivas filas de próximos índices primários.
-	Link aux2;
-	Queue aux3;
-	for(aux2 = coursesList->next; aux2 != NULL; aux2 = aux2->next){
-		printf("%s: %d\n", aux2->course, aux2->index);
-		for(aux3 = aux2->first; aux3 != NULL; aux3 = aux3->next){
-			printf("%d ", aux3->index);
-		}
-		printf("\n");
-	}
-	
-	fclose(input[0]);
-	fclose(input[1]);
-	fclose(output[0]);
-	fclose(output[1]);
-	return coursesList;
-}
-
-/*Salva um arquivo de índices secundários.*/
-void outputFile(Link list, FILE* output){
-	while(list != NULL){
-		fprintf(output, "%s: %d\n", list->course, list->index);
-		list = list->next;
+/*Retorna o índice existente na estrutura dos índices secundários.*/
+int findNextIndex(Link list, char* course, int index){
+	Queue aux;
+	while(list != NULL && strcmp(list->course, course) != 0) list = list->next;		//Busca pelo curso na lista de cursos.
+	if(list->index == index && list->first != NULL) 	//É o nó da lista e possui próximos índices.
+		return list->first->index;		//É o próprio nó da lista.
+	else if(list->first == NULL)		//Nó da lista de cursos não possui próximos índices.
+		return -1;
+	else{								//Procura na fila de próximos do nó da lista.
+		for(aux = list->first; aux != NULL && aux->index != index; aux = aux->next);
+		if(aux->next == NULL) 
+			return -1;
+		else 
+			return aux->next->index;
 	}
 }
 
@@ -271,19 +121,210 @@ void destroy(Link list){
 		while(list->first != NULL){
 			aux2 = list->first->next;
 			free(list->first);
+			list->first = NULL;
 			list->first = aux2;
 		}
 		free(list);
 		list = aux;
 	}
+	list = NULL;
+}
+
+void destroyText(char** txt, int totalLines){
+	for(int i = 0; i < totalLines; i++){
+		free(txt[i]);
+	}
+	free(txt);
+}
+
+char** createTxt(char* fileName, int* totalLines){
+	char aux;		//Variável auxiliar de leitura. Lê '\n' e garante a mudança de linha do ponteiro do arquivo.
+	
+	/*Inicialização do Arquivo de Entrada*/
+	FILE* input = fopen(fileName, "r");
+	while(input == NULL){
+		printf("Arquivo %s inexistente. Entre com o nome correto.\n", fileName);
+		scanf("%s", fileName);
+		input = fopen(fileName, "r");
+	}
+	
+	/*Contagem do número de linhas do arquivo de entrada.*/
+	while(!feof(input)){
+		aux = fgetc(input);
+		if(aux == '\n'){
+			*totalLines = *totalLines + 1;
+		}
+	}
+	
+	/*Volta o ponteiro ao início do arquivo de entrada.*/
+	rewind(input);
+	
+	char** txt = (char**)malloc(*totalLines * sizeof(char*));
+	for(int lineNumber = 0; lineNumber < *totalLines; lineNumber++){
+		txt[lineNumber] = (char*)malloc(X * sizeof(char));	//As linhas possuem X (constante) caracteres.
+		fscanf(input, "%[^\n]s", txt[lineNumber]);		//Lê uma linha completa.
+		fscanf(input, "%c", &aux);			//Lê '\n'
+	}
+	
+	fclose(input);
+	return txt;
+}
+
+/*FUNÇÃO AINDA INCOMPLETA.
+ *Entretanto, já imprime os índices primários.
+ */
+void primaryIndexes(char** txt, int totalLines, Link list){
+	int i = 0;		//Variável de navegação da string que contém uma linha inteira do arquivo de entrada.
+	int j = 0;		//Variável de navegação da string da chave primária.
+	int next;		//Armazena o próximo índice para o registro com a mesma chave secundária.
+	int flag = 0;	//Flag indica se um espaço ' ' foi lido duas vezes seguidas.
+	int lineNumber;	//Esta variável indica o número da linha atual lida nos arquivos lista1.txt ou lista2.txt.
+	char primaryKey[30];	//String que contém a CHAVE PRIMÁRIA concatenada com 30 caracteres.
+	char course[2];	//Armazena o curso lido por line nas posições 52 e 53.
+	char outputName[20];	//String do nome do arquivo de saída output.
+	
+	
+	/*Inicialização dos Arquivos de Saída*/
+	printf("Entre com o nome do arquivo de saida dos indices primarios da mesma lista: ");
+	scanf("%s", outputName);
+	FILE* output = fopen(outputName, "w");
+	
+	//Criação do Arquivo de Índices Primários do Arquivo lista1.txt
+	for(lineNumber = 0;  lineNumber < totalLines; lineNumber++){	//Lê as Y1 linhas do arquivo lista1.txt
+		while(j < 30 && i < X && flag < 2){					//Concatenação de 30 primeiros caracteres diferentes de ' '.
+			if(txt[lineNumber][i] != ' '){					//Continua a concatenação enquanto j < 30, i < X e flag não indica leitura de dois espaços seguidos.
+				primaryKey[j] = txt[lineNumber][i];			//Copia caractere da linha para a chave primária
+				i++;										//Incrementa o navegador da linha.
+				j++;										//Incrementa o navegador da chave primária.
+				flag = 0;									//Seta a flag em 0, já que não foram detectados dois ' ' seguidos.
+			}
+			else{
+				i++;										//Incrementa o navegador da linha.
+				flag++;										//Detectado 1 espaço, incrementa.
+			}
+		}
+		while(j < 30){										//Caso a string da chave não tenha 30 caracteres, completa com espaços.
+			primaryKey[j] = ' ';
+			j++;
+		}
+		primaryKey[30] = '\0';								//Caractere de final da string.
+		
+		course[0] = txt[lineNumber][52];					//Curso está nas posições 52 e 53. '\0' termina a string.
+		course[1] = txt[lineNumber][53];
+		course[2] = '\0';
+		
+		next = findNextIndex(list, course, lineNumber);
+		
+		fprintf(output, "%d\t%s\t%d\t%d\n", lineNumber, primaryKey, lineNumber, next);	//Imprime: "índicePrimário chavePrimária NRR" no arquivo de saída.
+		i = 0;												//Reinicia variáveis de navegação e flag.
+		j = 0;
+		flag = 0;
+	}
+	
+	fclose(output);
+}
+
+/*FUNÇÃO AINDA INCOMPLETA.
+ *A criação da estrutura de índices secundário funciona, mas só os imprime na tela.
+ *TODO: Estender a criação dos índices para o arquivo lista2.txt e imprimir nos arquivos de saída.
+ *      Verificar integração com "primaryIndexes".
+ */
+Link secondaryIndexes(char** txt, int totalLines){
+	Link aux;
+	char course[2];
+	int lineNumber;
+	char outputName[20];
+	
+	/*Inicialização do Arquivo de Saída*/
+	printf("Entre com o nome do arquivo de saida dos indices secundarios: ");
+	scanf("%s", outputName);
+	FILE* output = fopen(outputName, "w");
+	
+	/*Inicialização da Lista Encadeada de Cursos*/
+	/*Criação da cabeça da lista.*/
+	Link coursesList = (Link)malloc(sizeof(*coursesList));
+	coursesList->index = -1;
+	coursesList->next = NULL;
+	coursesList->first = NULL;
+	coursesList->last = NULL;
+
+	for(lineNumber = 0; lineNumber < totalLines; lineNumber++){
+		
+		//As posições 52 e 53 contêm a string do curso.
+		course[0] = txt[lineNumber][52];
+		course[1] = txt[lineNumber][53];
+		course[2] = '\0';
+		
+		if(checkCourse(coursesList->next, course)){	//Verifica se o curso lido já existe na lista de cursos.
+			//Caso positivo, procura o nó que contém o curso desejado e coloca o índice primário na fila.
+			insertQueue(coursesList->next, course, lineNumber);
+		}
+		else{	//Caso o curso não exista na lista, cria-se um novo nó contendo o curso lido, incluindo o primeiro índice de ocorrência do curso no arquivo.
+			coursesList->next = insertLink(coursesList->next, course, lineNumber);
+		}
+	}
+	
+	/*Impressão da Lista de Cursos e da fila de ponteiros na tela*/
+	//Percorre toda a lista de cursos e suas respectivas filas de próximos índices primários.
+	/*Link aux2;
+	Queue aux3;
+	for(aux2 = coursesList->next; aux2 != NULL; aux2 = aux2->next){
+		printf("%s: %d\n", aux2->course, aux2->index);
+		for(aux3 = aux2->first; aux3 != NULL; aux3 = aux3->next){
+			printf("%d ", aux3->index);
+		}
+		printf("\n");
+	}*/
+
+	aux = coursesList->next;
+	while(aux != NULL){
+		fprintf(output, "%s:\t%d\n", aux->course, aux->index);
+		aux = aux->next;
+	}
+
+	fclose(output);
+	return coursesList;
 }
 
 
 int main(){
+	char file1Name[30], file2Name[30];
+	char** txt1;
+	char** txt2;
+	int totalLines1;
+	int totalLines2;
 	
-	Link l = secondaryIndexes();
+	/**************** Lista1.txt **************/
+	printf("Entre com o nome do arquivo 1 de entrada: ");
+	scanf("%s", file1Name);
 	
-	destroy(l);
+	totalLines1 = 0;
+	txt1 = createTxt(file1Name, &totalLines1);
+	
+	Link l1 = secondaryIndexes(txt1, totalLines1);
+	
+	primaryIndexes(txt1, totalLines1, l1->next);
+	
+	destroyText(txt1, totalLines1);
+	destroy(l1->next);
+	free(l1);
+	l1 = NULL;
+	
+	/**************** Lista2.txt **************/
+	printf("Entre com o nome do arquivo 2 de entrada: ");
+	scanf("%s", file2Name);
+	
+	totalLines2 = 0;
+	txt2 = createTxt(file2Name, &totalLines2);
+
+	Link l2 = secondaryIndexes(txt2, totalLines2);
+	
+	primaryIndexes(txt2, totalLines2, l2->next);
+	
+	destroyText(txt2, totalLines2);
+	destroy(l2->next);
+	free(l2);
+	l2 = NULL;
 	
 	return 0;
 }
